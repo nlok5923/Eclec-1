30320941a18662837eeccde4c24a368c4ba1ee64
Hariom868 <54442410+Hariom868@users.noreply.github.com>
2020-12-18 18:49:37 +0530

    update value

--- PHP/delete.php
+++ PHP/delete.php
@@ -1,20 +1,20 @@
 <?php
 $user=$_GET['a']; // it save the regdate which is passed when delete button is clicked
-define('server','localhost:3307');// defining hostname
+define('server','localhost');// defining hostname
 define('username','root'); // defining username
 define('password' ,''); // defining Password
-define('databasename', 'NHDD');// defining database name
+define('databasename', 'Eclec');// defining database name
 $connection = mysqli_connect(server,username,password,databasename);
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 
-$sql = "DELETE FROM expenses WHERE RegDate = '$user'";//the sql command uses to delete row from expenses where the regdate matches
+$sql = "DELETE FROM equipment WHERE RegDate = '$user'";//the sql command uses to delete row from expenses where the regdate matches
 
 if (mysqli_query($connection, $sql)) {
     mysqli_close($connection);// it close the connection and move to the expense page
-    header('Location: view_expense_categorywise.php');
+    header('Location: usage.php');
     exit;
 } else {
     echo "Error deleting record";
